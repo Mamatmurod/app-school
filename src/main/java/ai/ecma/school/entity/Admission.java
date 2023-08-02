da7dea92098ea68efa8fd0dc2a1c8a08469903e5
asrorbek07 <asrorbekazamjon@gmail.com>
2023-08-02 12:36:09 +0500

    grouplevelenum added to admission

--- src/main/java/ai/ecma/school/entity/Admission.java
+++ src/main/java/ai/ecma/school/entity/Admission.java
@@ -1,6 +1,7 @@
 package ai.ecma.school.entity;
 
 import ai.ecma.school.entity.template.AbsUUIDUserAuditEntity;
+import ai.ecma.school.enums.GroupLevelEnum;
 import ai.ecma.school.enums.WeekdayEnum;
 import ai.ecma.school.utils.ColumnKey;
 import ai.ecma.school.utils.TableNameConstant;
@@ -43,6 +44,9 @@ public class Admission extends AbsUUIDUserAuditEntity {
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = ColumnKey.GROUP_TYPE_ID)
     private GroupType groupType;
+    @Enumerated(EnumType.STRING)
+    @Column(name=ColumnKey.LEVEL)
+    private GroupLevelEnum groupLevelEnum;
 
     /**
      * GURUHNING QAYSI MUTAXASISLIKKA TEGISHLI EKANLIGI
