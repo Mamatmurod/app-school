30433c8204d86df85ce1fde315b26363fce81874
kamron <bozorvkamron@gmail.com>
2023-08-03 11:15:36 +0500

    admission

--- src/main/java/ai/ecma/school/payload/AdmissionDTO.java
+++ src/main/java/ai/ecma/school/payload/AdmissionDTO.java
@@ -1,14 +1,18 @@
 package ai.ecma.school.payload;
 
-import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 public class AdmissionDTO {
 
-
+    private UUID id;
+    private Double totalPrice;
+    private Double discountPrice;
+    private Byte discountPricePercentage;
     private String groupLevel;
     private Long starDate;
     private Long endDate;
