cd858a24283053f20ac88e0b8a1649d79181e7b4
kamron <bozorvkamron@gmail.com>
2023-08-04 10:54:33 +0500

    Level

--- src/main/java/ai/ecma/school/enums/GroupLevelEnum.java
+++ src/main/java/ai/ecma/school/enums/GroupLevelEnum.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum GroupLevelEnum {
-    V(5),
-    VI(6),
-    VII(7),
-    VIII(8),
-    IX(9),
-    X(10),
-    XI(11);
+    V("5"),
+    VI("6"),
+    VII("7"),
+    VIII("8"),
+    IX("9"),
+    X("10"),
+    XI("11");
 
-    private final Integer level;
+    private final String level;
 }
--- src/main/java/ai/ecma/school/repository/AdmissionRepository.java
+++ src/main/java/ai/ecma/school/repository/AdmissionRepository.java
@@ -1,16 +1,16 @@
 package ai.ecma.school.repository;
 
 import ai.ecma.school.entity.Admission;
+import ai.ecma.school.enums.GroupLevelEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AdmissionRepository extends JpaRepository<Admission, UUID> {
-    Boolean findByGroupLevelEnum(String groupLevelEnum);
+    Boolean existsAdmissionByLevel_LevelEnum(GroupLevelEnum level_levelEnum);
 
     List<Admission> findAllByIsDeleted(boolean isDeleted);
 }
